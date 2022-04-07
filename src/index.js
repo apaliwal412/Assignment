6832cfc123e112a024a5d5685610fcf034e9d851
apaliwal412 <53301629+apaliwal412@users.noreply.github.com>
2022-04-07 16:13:06 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
+
+import { createStore } from 'redux';
+
+const myStore = createStore(allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
+ReactDOM.render(
+  <Provider store={myStore}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+reportWebVitals();
